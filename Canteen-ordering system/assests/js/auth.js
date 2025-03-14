bb65b683ff0ca433d295c2acf5c72eed10a1b27d
S1ddheshh <s1ddhh.06@gmail.com>
2025-03-14 12:49:43 +0530

    firebase auth

--- Canteen-ordering system/assests/js/auth.js
+++ Canteen-ordering system/assests/js/auth.js
@@ -1,5 +1,30 @@
 /* user login */
 // Student Login Form Submission
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAiJplG7u_x_ijhxC9AD9F2JPYoU0k2Lnk",
+  authDomain: "online-cateen-ordeing.firebaseapp.com",
+  projectId: "online-cateen-ordeing",
+  storageBucket: "online-cateen-ordeing.appspot.com",
+  messagingSenderId: "1088334175531",
+  appId: "1:1088334175531:web:586611bcf1bbba05f5f96e",
+  measurementId: "G-BL8BCFH88B"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+// Logout Function
+function logout() {
+  auth.signOut().then(() => {
+    window.location.href = '../auth/user/login.html'; // or admin/login.html
+  }).catch((error) => {
+    alert('Error logging out: ' + error.message);
+  });
+}
+
 document.getElementById('student-login-form').addEventListener('submit', function (e) {
   e.preventDefault();
   const email = document.getElementById('student-email').value;
